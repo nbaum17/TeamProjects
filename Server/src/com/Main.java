2e85f5aed91eeeda0ee7efce2d2fc4a7a22bfd0b
Anthony Drow <antwonardo@gmail.com>
2017-10-22 13:39:39 -0400

    Created MySQLConnection object for database

---
+++ Server/src/com/Main.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Main 
+{
+	  public static void main(String[] args) throws Exception 
+	  {
+	    MySQLConnection dao = new MySQLConnection();
+	    dao.readDataBase();
+	  }
+}
